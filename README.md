cb8212e58df5bc7f9b00e50631770d5be9114ec3
Fred <fred@apigoat.com>
2020-09-14 14:29:54 +0800

    initial

---
+++ README.md
@@ -0,0 +1,39 @@
+# A class to write Html in PHP
+
+I hate HTML! I hate putting multiple language is PHP files. So I use a small class that produces HTML and add some shortcut.
+
+So I wrie HTML like that:
+
+```
+$Html = new Html('bootstrap', true);
+
+$Html
+    ->jumbotron(null)
+        ->addH1("Use HJSON intead of XML for Propel")
+        ->addP("This library is part of APIgoat.")
+    ->close()
+    ->div(null, ['class' => 'card-group'])
+        ->div(null, ['class' => 'card'])
+            ->div(null, ['class' => 'card-header'])
+                ->addSpan("HJSON to Propel XML schema.", ["style" => "display: inline-block;width: 49%;"])
+                ->div(null, ["style" => "display: inline-block;width: 50%;text-align: right;"])
+                    ->addBut("Convert", ['id' => 'convert', 'type' => 'primary'])
+            ->close()
+        ->close()
+        ->div(null, ['class' => 'card-body'])
+            ->addDiv("", ['id' => 'hjson-editor', "style" => "height: 100%; width: 100%"])
+        ->close('all', 1)
+        ->div(null, ['class' => 'card'])
+            ->div(null, ['class' => 'card-header'])
+                ->addSpan("HJSON to Propel XML schema.", ["style" => "display: inline-block;width: 49%;"])
+                ->div(null, ["style" => "display: inline-block;width: 50%;text-align: right;"])
+                ->addBut("Download schema", ['id' => 'download', 'type' => 'primary'])
+            ->close()
+        ->close()
+        ->div(null, ['class' => 'card-body'])
+        ->addDiv("", ['id' => 'xml-editor', 'addclass' => 'editor', "style" => "height: 100%; width: 100%"])
+    ->close('all');
+
+    $html =  $Html->body(null, ['class' => 'body'])->addContainerFull($Html->getBuffer())->close()->getPage();
+
+```
---
+++ src/Html.php
@@ -0,0 +1,315 @@
+<?php
+
+namespace App\Application\Helper\Html;
+
+class Html
+{
+
+    /**
+     * default class to use on tags
+     *
+     * @var array
+     */
+    private $classes =
+    [
+        'bootstrap' =>
+        [
+            'panel' => 'card-group',
+            'panel-default' => 'card',
+            'panel-heading' => 'card-header',
+            'panel-body' => 'card-body',
+            'container' => 'container',
+            'container-fluid' => 'container-fluid',
+            'jumbotron' => 'jumbotron',
+            'row' => 'row',
+            'col' => 'col-lg-',
+            'button' => 'btn btn-outline-primary',
+            'button-primary' => 'btn btn-lg btn-primary',
+            'h1' => '',
+            'h2' => '',
+            'h3' => '',
+            'ul' => '',
+            'li' => '',
+            'table' => 'table',
+
+        ]
+    ];
+    private $id = 1;
+
+    /**
+     * html buffers
+     *
+     * @var string
+     */
+    private $head = '';
+    private $html = '';
+    private $buffer = '';
+    private $toClose = [];
+
+    /**
+     * Markups that refers to inputs where content should be in value
+     */
+    private $inputs = [
+        "input",
+        "button",
+        "checkbox",
+        "textarea"
+    ];
+
+    /**
+     * Produce human readable code
+     *
+     * @var boolean
+     */
+    private $humanReadable = true;
+
+    /**
+     * add incremental id to tags
+     *
+     * @var boolean
+     */
+    private $autoId = true;
+
+    /**
+     * add incremental id to tags
+     *
+     * @var boolean
+     */
+    private $autoHtml = true;
+
+    public function __construct($framework = '', $buffer = false)
+    {
+        $this->isBuffered = false;
+        if ($buffer) {
+            $this->isBuffered = true;
+        }
+
+        $this->class = $this->classes[$framework];
+
+        if ($this->humanReadable) {
+            $this->newline = \PHP_EOL;
+        } else {
+            $this->newline = "";
+        }
+    }
+
+    public function __call($method, $args): Object
+    {
+        if (\preg_match('/^(add)(\w*)/', $method, $match)) {
+            $method = strtolower($match[2]);
+            $close = true;
+        } else {
+            $close = false;
+        }
+
+        if (\method_exists($this, $method)) {
+            $this->$method($close, $args[0], $args[1]);
+        } else if (\method_exists($this, 'add' . \ucfirst($method))) {
+            $this->$method($close, $args[0], $args[1]);
+        } else {
+            $this->markup($method, $close, $args[0], $args[1]);
+        }
+
+        return $this;
+    }
+
+    private function getOptions($markup, $options): string
+    {
+
+        if (!empty($this->class[$markup]) && !isset($options['class'])) {
+            $options = array_merge($options, ['class' => $this->class[$markup]]);
+        }
+
+        if (isset($options['addclass'])) {
+            $options = array_merge($options, ['class' => $options['addclass']]);
+            unset($options['addclass']);
+        }
+
+        if ($this->autoId) {
+            if (!isset($options['id'])) {
+                $options['id'] = 'auto' . $this->getNextId();
+            }
+        }
+
+        return $this->array2options($options);
+    }
+
+    private function markup($markup, $close, $content = '', $options = []): Object
+    {
+
+        $options = $this->getOptions($markup, $options);
+
+        if (in_array($markup, $this->inputs) && !isset($options['value'])) {
+            $options['value'] = $content;
+            $content = '';
+        }
+
+        if ($close) {
+            $this->buffer .= $this->indent() . "<{$markup} " . $options . ">{$content}</{$markup}>{$this->newline}";
+        } else {
+
+            array_unshift($this->toClose, $markup);
+            $this->buffer .= $this->indent() . "<{$markup} " . $options . ">{$this->newline}";
+        }
+
+        return $this;
+    }
+
+    private function indent(): string
+    {
+        if ($this->humanReadable) {
+            return str_repeat("\t", count($this->toClose));
+        } else {
+            return "";
+        }
+    }
+
+    public function getBuffer(): string
+    {
+        $buffer = $this->buffer;
+        $this->buffer = '';
+        return $buffer;
+    }
+
+    public function close($all = false, $except = 0)
+    {
+        if ($all && count($this->toClose)) {
+            $toclose = count($this->toClose) - $except;
+            for ($i = 0; $i < $toclose; $i++) {
+                $markup = array_pop($this->toClose);
+                $this->buffer .= $this->indent() . "</{$markup}>{$this->newline}";
+            }
+        } else {
+            $markup = array_pop($this->toClose);
+            $this->buffer .= $this->indent() . "</{$markup}>{$this->newline}";
+        }
+
+        return $this;
+    }
+
+    public function getNextId()
+    {
+        return $this->id++;
+    }
+
+    /**
+     * Output
+     */
+
+    public function getPage(): string
+    {
+        if ($this->autoHtml) {
+            $this->html .= "<!DOCTYPE html>{$this->newline}<html>{$this->newline}" . $this->buffer . "{$this->newline}</html>";
+        } else {
+            $this->html .= $this->buffer;
+        }
+
+        return $this->getHtml(true, true);
+    }
+
+    public function getHtml($reset = true, $head = false)
+    {
+        if ($this->bodyOpen) {
+            $this->html .= "</body>{$this->newline}";
+        }
+
+        $html = $this->html;
+        if ($reset) {
+            $this->html = "";
+        }
+
+        return (($head) ? $this->head : '') . $html;
+    }
+
+
+    /**
+     * Shortcuts
+     */
+
+    function preprint($in)
+    {
+        $this->addPre(print_r($in, true));
+        echo $this->getHtml();
+    }
+
+    private function array2options($options)
+    {
+        $optionsContent = "";
+
+        if (is_array($options)) {
+            foreach ($options as $key => $data) {
+                if (is_array($data)) {
+                    foreach ($data as $subkey => $subdata) {
+                        $optionsContent .= $key . "-" . $subkey . '="' . $subdata . '" ';
+                    }
+                } else {
+                    $optionsContent .= $key . '="' . $data . '" ';
+                }
+            }
+
+            return $optionsContent;
+        } else {
+            return "";
+        }
+    }
+
+    public function jumbotron($close, $content = null)
+    {
+        $this->markup('div', $close, $content, ['class' => $this->class['jumbotron']]);
+        return $this;
+    }
+
+    public function container($close, $content = null)
+    {
+        $this->markup('div', $close, $content, ['class' => $this->class['container']]);
+        return $this;
+    }
+
+    public function containerFull($close, $content = null)
+    {
+        $this->markup('div', $close, $content, ['class' => $this->class['container-fluid']]);
+        return $this;
+    }
+
+    public function but($close, $content = "", $options = [])
+    {
+
+        if (isset($this->class['button'])) {
+            $options['class'] = $this->class['button'];
+        }
+
+        if (!isset($options['type']))
+            $options['type'] = 'default';
+
+        if (!isset($options['href']))
+            $options['href'] =  "Javascript:;";
+
+
+        return $this->markup('a', $close, $content, $options);
+    }
+
+    function addHead($options)
+    {
+        $this->head = "<head>";
+        $this->head .= "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
+        if (!empty($options['desciption']))
+            $this->head .= "<meta name=\"description\" content=\"" . $options['desciption'] . "\" />";
+        if (!empty($options['keywords']))
+            $this->head .= "<meta name=\"keywords\" content=\"" . $options['keywords'] . "\" />";
+        if (!empty($options['title']))
+            $this->head .= "<title>" . $options['title'] . "</title>";
+        if (!empty($options['favicon']))
+            $this->head .= "<link rel=\"icon\" type=\"image/png\" href=\"" . _PUBLIC_DIR . $options['favicon'] . "\" />";
+        if (!empty($options['style']))
+            $this->head .= "\n" . $options['style'];
+        if (!empty($options['script']))
+            $this->head .= $options['script'];
+        $this->head .= $options['others'];
+        $this->head .= "<meta name='Author' content='" . $options['author'] . "' />";
+        $this->head .= "<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>";
+
+        $this->head .= "</head>{$this->newline}";
+
+        return $this;
+    }
+}
